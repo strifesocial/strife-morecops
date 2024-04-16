f2541f88853df26117a4c5c0a28fcbaa329f4d1a
Strife Social <166969842+strifesocial@users.noreply.github.com>
2024-04-16 15:11:09 -0400

    Updated - fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Strife Social'
-description 'More Cops Script'
+author 'Strife Social & Strife Social Developments'
+description 'More cops script.'
+
 version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
-shared_script 'config.lua'
 
-lua5.4 'yes'
+lua54 'yes'
